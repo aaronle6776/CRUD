839db6e07fdf87147b6232a631a1f4b37bb710c9
Aaronle6776 <malmizal6776@naver.com>
2019-05-23 21:57:22 +0900

    hi

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CRUD.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['CRUD/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
